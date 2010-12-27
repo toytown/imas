3a9a89b92ed8d05f763a671ce1f149263ed50e50
prasanna.tuladhar <prasanna.tuladhar@WS0668.wirecard.lan>
2010-12-27 12:56:05 +0100

    added test case

---
+++ imas/src/test/java/com/imas/dao/impl/CommonDaoImplTest.java
@@ -0,0 +1,25 @@
+package com.imas.dao.impl;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommonDaoImplTest {
+
+    @Test
+    public void testGetTypes() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetHeatingTypes() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCategories() {
+        fail("Not yet implemented");
+    }
+
+}
